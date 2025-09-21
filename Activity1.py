473a0c80a716b90636994c0789620f369dda0206
Vivienne126 <vivienne.singh@cns.ac.in>
2025-09-21 14:01:18 +0530

    New1

--- Activity1.py
+++ Activity1.py
@@ -12,11 +12,19 @@ def apply_filter(image,filter_type):
     elif filter_type=="green":
         fimg[:,:,0]=0 #Blue channel to zero
         fimg[:,:,2]=0 #Red channel to zero
-    elif filter_type=="increase red":
-        fimg[:,:,2]=cv2.add(fimg[:,:,2] , 50)
-    elif filter_type=="decrease blue":
-        fimg[:,:,0]=cv2.subtract(fimg[:,:,0] , 50)
+    elif filter_type=="sobel":
+        grayimage=cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+        sobelx=cv2.Sobel(grayimage, cv2.CV_64F , 1 , 0 , ksize=3)
+        sobely=cv2.Sobel(grayimage, cv2.CV_64F , 0 , 1 , ksize=3)
+        combine_sobel=cv2.bitwise_or(sobelx.astype("uint8") , sobely.astype("uint8"))
+        fimg=cv2.cvtColor(combine_sobel , cv2.COLOR_GRAY2BGR)
+    elif filter_type=="canny":
+        grayimage=cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+        edges=cv2.Canny(grayimage , 100, 200)
+        fimg=cv2.cvtColor(edges , cv2.COLOR_GRAY2BGR)
     return fimg
+
+    
 
 image_path="example.jpg"
 image=cv2.imread(image_path)
@@ -28,8 +36,8 @@ else:
     print("r for red tint")
     print("b for blue tint")
     print("g for green tint")
-    print("i for increase red intensity")
-    print("d for decrease blue intensity")
+    print("s for sobel")
+    print("c for canny")
     print("press q for quit")
     while True:
         fimg=apply_filter(image,filter_type)
@@ -41,10 +49,10 @@ else:
             filter_type="blue"   
         elif key==ord("g"):
             filter_type="green"  
-        elif key==ord("i"):
-            filter_type="increase red"
-        elif key==ord("d"):
-            filter_type="decrease blue"    
+        elif key==ord("s"):
+            filter_type="sobel"
+        elif key==ord("c"):
+            filter_type="canny"    
         elif key==ord("q"):
             print("Exiting...")
             break
